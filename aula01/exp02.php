a97db6e7e078bf994f15c475ceef5975919cbb84
arthurscqr <146879768+arthurscqr@users.noreply.github.com>
2023-10-17 16:20:36 -0300

    Add files via upload

---
+++ aula01/exp02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXP 02</title>
+</head>
+<body>
+    <?php
+    $color = "BLUE";
+
+    echo "My car is" . $color . "<br>";
+    echo "My car is" . $COLOR . "<br>";
+    echo "My car is" . $coLOR . "<br>";
+
+    ?>
+</body>
+</html>
---
+++ aula01/exp05.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXP 05</title>
+</head>
+<body>
+    <?php
+    $curso = "WEB Médio - Aluno 01!";
+    $disciplina = "PSW01";
+    $turma = "2BII/2023";
+    
+    echo "[CURSO]: $curso <br>";
+    echo "[DISCIPLINA] $disciplina <br>";
+    echo "[TURMA] $turma <br>";
+
+    echo "<br> This", "<br> String", "<br> Was", "<br> Made", "<br> With Multiple Parameters";
+    ?>
+</body>
+</html>
